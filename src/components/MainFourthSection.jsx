5ab47baa530f7ec2b13824ffff5c393264910bcd
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-09-06 19:29:21 -0300

    Quarta parte do MainContent.

--- src/components/MainFourthSection.jsx
+++ src/components/MainFourthSection.jsx
@@ -1,7 +1,18 @@
+import FourthSectionPNG from '../images/ImageSection4.png';
+
 function MainFourthSection() {
   return (
     <section className="fourth_section">
-      Hi
+      <h2>
+        A framework built for the long term
+      </h2>
+      <p>
+        Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.
+      </p>
+      <a href="https://" className="app_links">
+        Read how Hapu’s tribal background defines our app 
+      </a>
+      <img src={ FourthSectionPNG } alt="example: user billing history" />
     </section>
   );
 }
